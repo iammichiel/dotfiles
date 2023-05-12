8a224d3706a6f35a0961231a9c290f19d0480689
Michiel Missotten <michiel.missotten@protonmail.com>
2023-05-12 15:29:50 +0200

    Adding a defautl global_gitignore and adding ImageOptim tool

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ brew bundle --file ~/.dotfiles/homebrew/Brewfile
 
 echo "Setting up Oh-my-zsh configuration file..."
 ln -s -f ~/.dotfiles/zsh/zshrc ~/.zshrc
+
+echo "Configuration of Git..."
+git config --global core.excludesfile ~/.dotfiles/git/global_gitignore
