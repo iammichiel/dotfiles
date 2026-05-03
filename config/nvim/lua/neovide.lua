aaa38c55e72e35157e1442f6e9592c881ec98820
Michiel Missotten <michiel.missotten@decathlontravel.com>
2026-05-03 13:59:56 +0200

    Defaulting Neovide to workspace folder.

--- config/nvim/lua/neovide.lua
+++ config/nvim/lua/neovide.lua
@@ -2,9 +2,19 @@ vim.g.neovide_scale_factor = 1.0
 local change_scale_factor = function(delta)
     vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
+
 vim.keymap.set("n", "<C-=>", function()
     change_scale_factor(1.25)
 end)
+
 vim.keymap.set("n", "<C-->", function()
     change_scale_factor(1 / 1.25)
 end)
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        if vim.fn.argc() == 0 then
+            vim.cmd("cd ~/workspace")
+        end
+    end,
+})
